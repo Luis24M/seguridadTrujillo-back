278f7d923a6726caf476b6f7d03e008e13aa8746
Lui24 <gnu936@gmail.com>
2024-08-08 20:50:38 -0500

    ;-;

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sequelize = new Sequelize(
     process.env.MYSQL_DATABASE,
     process.env.MYSQLUSER,
-    process.env.MYSQLPASSWORD
+    process.env.MYSQLPASSWORD,
     {
         host: process.env.MYSQLHOST,
         port: process.env.MYSQLPORT,
