a9b57d7cacdce66d0c2b0cacaba6048f7011e03c
Luis Morales <79761378+Luis24M@users.noreply.github.com>
2024-08-11 19:03:50 -0500

    Update Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -2,8 +2,8 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Usuario = sequelize.define('Usuario', {
-    id_usuario: {
-        type: DataTypes.INTEGER,
+    uid: {
+        type: DataTypes.STRING(100),
         primaryKey: true,
         autoIncrement: true,
     },
